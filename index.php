f7657a99eaf7ca6ed6203f3c572e7368361fb30d
krpatel21 <krpatel21@outlook.com>
2015-11-23 18:52:29 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- Easy Sell Application -->
+
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>EasySell: Home Page</title>
+        <link href="TitleImage1.png" rel="icon">
+        <link href="EasySellStyleSheet.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+    	<header id="header">
+        	<img id = "easySellLogo" src="easySell_Logo.png" alt="easySellLogo" height="100px">
+        
+        </header>
+        
+        <div id="content">
+        	<div id="leftSideBar">
+            	<form id="newsearch" method="get" action="#">
+		        	<input type="text" id="searchText" width="100px" name="search" size="21" maxlength="100" value="Search our website">
+                    <input id = "buttons" type = "button" value = "Search">
+                    
+				</form>
+
+            
+            
+            </div>
+        	
+     
+        </div>
+        
+        <footer id="footer">
+        	<img style="float:right; padding-right:25px;" id = "kppTechLogo" src="KPP_Logo.png" alt="kppTechLogo" height="50px">
+        </footer>
+    
+	</body>
+</html>
